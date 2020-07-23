c6d422c721f98f8fcb089f838aabc9a585881881
broccolai <josh@magmo.co.uk>
2020-07-23 01:39:37 +0100

    Add tooling

---
+++ next-env.d.ts
@@ -0,0 +1,40 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+
+interface PropertiesItemType {
+  startValue: string | number;
+  endValue: string | number;
+  property: string;
+  unit?: string;
+}
+
+interface ParallaxDataType {
+  start: string | number | SafeHTMLElement;
+  startOffset?: string | number;
+  duration?: string | number | SafeHTMLElement;
+  end?: string | number | SafeHTMLElement;
+  endOffset?: string | number;
+  properties: PropertiesItemType[];
+  easing?: string | array | func;
+  name?: string;
+}
+
+interface IPlxPros {
+  animateWhenNotInViewport?: boolean;
+  children?: any;
+  className?: string;
+  disabled?: boolean;
+  freeze?: boolean;
+  parallaxData: ParallaxDataType[];
+  style?: string | number | object;
+  tagName?: string;
+  onPlxStart?: PropTypes.func;
+  onPlxEnd?: PropTypes.func;
+}
+
+declare module 'react-plx' {
+  declare class Plx extends React.Component<IPlxPros> {
+    constructor(props: IPlxPros);
+  }
+  export = Plx;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import NextHead from 'next/head';
+import React from 'react';
+
+const Index = () => (
+  <>
+    <NextHead>
+      <meta charSet="UTF-8" />
+      <title>procursus</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </NextHead>
+    <h1>procursus</h1>
+  </>
+);
+
+export default Index;
